ad1df615be7b7febd24a0d849bcebf9cd489cdec
Hypecolt <59376021+Hypecolt@users.noreply.github.com>
2021-06-18 17:04:48 +0300

    Add files via upload

---
+++ SiteTW/JS/basics.js
@@ -0,0 +1,152 @@
+﻿var score = 0;
+
+var monsters = [
+    '/images/bigaxeguy.png',
+    '/images/goblin.png',
+    '/images/golem.png',
+    '/images/mummy.png',
+    '/images/zombie.png'
+]
+
+let items = [];
+
+function readXML() {
+    parser = new DOMParser();
+    var file = "/items_mobs/items.xml";
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                xmlDoc = parser.parseFromString(allText, "text/xml");
+                var sword = xmlDoc.getElementsByTagName("sword");
+                var armor = xmlDoc.getElementsByTagName("armor");
+                var legs = xmlDoc.getElementsByTagName("legs");
+                var helmet = xmlDoc.getElementsByTagName("helmet");
+                var boots = xmlDoc.getElementsByTagName("boots");
+                var shield = xmlDoc.getElementsByTagName("shield");
+                itemJSONformat(sword);
+                itemJSONformat(armor);
+                itemJSONformat(legs);
+                itemJSONformat(helmet);
+                itemJSONformat(boots);
+                itemJSONformat(shield);
+                return items;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function itemJSONformat(item) {
+    var i;
+    var itm;
+
+    for (i = 0; i < item.length; i++) {
+        itm = {
+            "name": item[i].getElementsByTagName("name")[0].childNodes[0].nodeValue,
+            "hp": item[i].getElementsByTagName("hp")[0].childNodes[0].nodeValue,
+            "mp": item[i].getElementsByTagName("mp")[0].childNodes[0].nodeValue,
+            "str": item[i].getElementsByTagName("str")[0].childNodes[0].nodeValue,
+            "int": item[i].getElementsByTagName("int")[0].childNodes[0].nodeValue,
+            "def": item[i].getElementsByTagName("def")[0].childNodes[0].nodeValue
+        };
+        items.push(itm);
+    }
+}
+
+function addItem() {
+
+    const idx = Math.floor(Math.random() * items.length) + 0;
+
+    var name = items[idx].name;
+    var hp = items[idx].hp;
+    var mp = items[idx].mp;
+    var str = items[idx].str;
+    var int = items[idx].int;
+    var def = items[idx].def;
+
+    var data = {
+        hp: hp,
+        mp: mp,
+        str: str,
+        int: int,
+        def: def
+    };
+
+    var itm = document.createElement("p");
+    itm.classList.add("equipped-items");
+    itm.innerHTML = name;
+
+    var itm_desc = itm.appendChild(document.createElement("span"));
+    itm_desc.classList.add("equipped-items-text");
+
+    var name_desc = itm_desc.appendChild(document.createElement("p"));
+    name_desc.innerHTML = name;
+    name_desc.classList.add("item-desc");
+
+    var hp_desc = itm_desc.appendChild(document.createElement("p"));
+    hp_desc.innerHTML = "HP - " + hp;
+    hp_desc.classList.add("item-desc");
+
+    var mp_desc = itm_desc.appendChild(document.createElement("p"));
+    mp_desc.innerHTML = "MP - " + mp;
+    mp_desc.classList.add("item-desc");
+
+    var str_desc = itm_desc.appendChild(document.createElement("p"));
+    str_desc.innerHTML = "STR - " + str;
+    str_desc.classList.add("item-desc");
+
+    var int_desc = itm_desc.appendChild(document.createElement("p"));
+    int_desc.innerHTML = "INT - " + int;
+    int_desc.classList.add("item-desc");
+
+    var def_desc = itm_desc.appendChild(document.createElement("p"));
+    def_desc.innerHTML = "DEF - " + def;
+    def_desc.classList.add("item-desc");
+
+    document.getElementById("inventory").appendChild(itm);
+
+    return data;
+}
+
+function fCanvas() {
+
+    var randomMob = monsters[Math.floor(Math.random() * monsters.length)];
+    var canvas = document.getElementById("gameScreen"),
+    context = canvas.getContext("2d");
+
+    base_image = new Image();
+    base_image.src = randomMob;
+
+    context.clearRect(50, -15, 200, 150);
+    context.drawImage(base_image, 50, -15, 200, 150);
+    
+}
+
+function test() {
+    var itm = addItem();
+    console.log(itm);
+}
+
+readXML();
+
+$(window).on('load', function () {
+
+    var i;
+    var canvas = document.getElementById("gameScreen"),
+        context = canvas.getContext("2d");
+
+    for(i = 0 ; i < 20; i++){
+
+        var randomMob = monsters[Math.floor(Math.random() * monsters.length)];
+
+        base_image = new Image();
+        base_image.src = randomMob;
+
+        context.clearRect(50, -15, 200, 150);
+        context.drawImage(base_image, 50, -15, 200, 150);
+    }
+
+});
---
+++ SiteTW/JS/character.js
@@ -0,0 +1,183 @@
+﻿var mana_spent = 0;
+
+class Character {
+    constructor(chp, hp, mp, str, int, def) {
+        this.chp = chp;
+        this.hp = hp;
+        this.mp = mp;
+        this.str = str;
+        this.int = int;
+        this.def = def;
+    }
+
+    initialize() {
+        document.getElementById("chp").innerHTML = this.chp;
+        document.getElementById("hp").innerHTML = this.hp;
+        document.getElementById("mp").innerHTML = this.mp;
+        document.getElementById("str").innerHTML = this.str;
+        document.getElementById("int").innerHTML = this.int;
+        document.getElementById("def").innerHTML = this.def;
+    }
+
+    newGame() {
+        this.chp = 100;
+        this.hp = 100;
+        this.mp = 50;
+        this.str = 5;
+        this.int = 5;
+        this.def = 5;
+        this.initialize();
+
+        document.getElementById("inventory").innerHTML = "";
+
+    }
+
+    save() {
+        var data = {
+            chp: this.chp,
+            hp: this.hp,
+            mp: this.mp,
+            str: this.str,
+            int: this.int,
+            def: this.def
+        };
+
+        var data_but_in_json = JSON.stringify(data);
+        localStorage.setItem("gameSave", data_but_in_json);
+        document.cookie = "mp = " + this.mp + ";"
+    }
+
+    load() {
+        var obj = JSON.parse(localStorage.getItem("gameSave"));
+        
+        this.chp = obj.chp;
+        this.hp = obj.hp;
+        this.mp = obj.mp;
+        this.str = obj.str;
+        this.int = obj.int;
+        this.def = obj.def;
+        this.initialize();
+    }
+
+    trainHP() {
+
+        if (this.mp > 0) {
+
+            this.hp = this.hp + 10;
+            this.chp = this.chp + 10;
+            document.getElementById("chp").innerHTML = this.chp;
+            document.getElementById("hp").innerHTML = this.hp;
+        }
+        this.spendMana();
+    }
+
+    trainSTR() {
+
+        if (this.mp > 0) {
+
+            this.str = this.str + 1;
+            document.getElementById("str").innerHTML = this.str;
+        }
+        this.spendMana();
+    }
+
+    trainINT() {
+
+        if (this.mp > 0) {
+
+            this.int = this.int + 1;
+            document.getElementById("int").innerHTML = this.int;
+            document.getElementById("mp").innerHTML = this.manaIncrease();
+        }
+        this.spendMana();
+    }
+
+    trainDEF() {
+
+        if (this.mp > 0) {
+
+            this.def = this.def + 1;
+            document.getElementById("def").innerHTML = this.def;
+        }
+        this.spendMana();
+    }
+
+    manaIncrease() {
+        this.mp = this.mp + 2;
+        return this.mp;
+    }
+
+    spendMana() {
+
+        var x = document.getElementById("no_mana");
+
+        if (this.mp === 0) {
+            x.style.visibility = "visible";
+        } else {
+            this.mp = this.mp - 1;
+            x.style.visibility = "hidden";
+            document.getElementById("mp").innerHTML = this.mp;
+
+            mana_spent = mana_spent + 1;
+            document.getElementById("mana-spent").innerHTML = mana_spent;
+            sessionStorage.setItem("mana_spent", mana_spent);
+        }
+    }
+
+    recoverMana() {
+
+        var x = document.getElementById("no_mana");
+
+        this.mp = this.mp + 5;
+        x.style.visibility = "hidden";
+        document.getElementById("mp").innerHTML = this.mp;
+        fCanvas();
+    }
+
+    addItemStats() {
+
+        fCanvas();
+
+        let item = addItem();
+
+        var hp = parseInt(item.hp);
+        var mp = parseInt(item.mp);
+        var str = parseInt(item.str);
+        var int = parseInt(item.int);
+        var def = parseInt(item.def);
+
+
+        this.chp = this.chp + hp;
+        this.hp = this.hp + hp;
+        this.mp = this.mp + mp;
+        this.str = this.str + str;
+        this.int = this.int + int;
+        this.def = this.def + def;
+
+        this.initialize();
+
+    }
+
+}
+
+let charac = new Character();
+
+$(window).on('load', function () {
+    try {
+        charac.load();
+    }
+    catch (err) {
+        charac.newGame();
+    }
+
+    if (sessionStorage.getItem('mana_spent')) {
+        document.getElementById("mana-spent").innerHTML = sessionStorage.getItem('mana_spent');
+        mana_spent = parseInt(sessionStorage.getItem('mana_spent'));
+        console.log(mana_spent);
+    }
+    else {
+        document.getElementById("mana-spent").innerHTML = mana_spent;
+        console.log(mana_spent);
+    }
+
+});
